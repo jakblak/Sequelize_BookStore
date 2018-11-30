939f12c128bf5d8c60da2966e475d69b7cac8277
Steve Mustanski <steve.mustanski@gmail.com>
2018-11-30 17:25:47 -0500

    fix for issue #2 - missing slash on /api/users route in index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
 // api router will mount other routers
 module.exports = (app) => {
   app.get('/', (req, res) => { res.send('hello world') });
-  app.use('api/users', require('./user/user.routes'));
+  app.use('/api/users', require('./user/user.routes'));
 }
